751bde1dac09756d3b7f2bcb84a03acd475d7d8b
AlexanderVlDubinin <alexander.vl.dubinin@gmail.com>
2026-05-31 18:36:56 +0300

    Corrections.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+umask(0000); // Set the umask to 0000 to ensure the files are created with the correct permissions
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
